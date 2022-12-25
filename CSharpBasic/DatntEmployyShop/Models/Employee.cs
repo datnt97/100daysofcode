4bbffbced4607a19e32b87a904cd9630debfe736
D97 <datnt3009@gmail.com>
2022-12-26 05:10:39 +0700

    Readd source

---
+++ CSharpBasic/DatntEmployyShop/Models/Employee.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Emit;
+
+public class Employee
+{
+	private string firstName;
+	private string lastName;
+
+	private int numberOfHoursWorked;
+	private double wage;
+	private double hourlyRate;
+
+	public string FirstName
+	{
+		get => firstName;
+		set => firstName = value;
+	}
+
+	public string LastName
+	{
+		get { return lastName; }
+		set
+		{
+			lastName = value;
+		}
+	}
+
+	public int NumberOfHoursWorked
+	{
+		get
+		{
+			return numberOfHoursWorked;
+		}
+
+		set
+		{
+			numberOfHoursWorked = value;
+		}
+	}
+
+	public double Wage
+	{
+		get => numberOfHoursWorked * hourlyRate;
+	}
+
+	public double HourlyRate
+	{
+		get { return hourlyRate; }
+		set
+		{
+			hourlyRate = value;
+		}
+	}
+
+	public string FullName => $"{firstName} {lastName}";
+
+	public Employee(string firstName, string lastName, double hourlyRate)
+	{
+		FirstName = firstName;
+		LastName = lastName;
+		HourlyRate = hourlyRate;
+	}
+
+	public virtual void PerformWork(int hoursWorked)
+	{
+		NumberOfHoursWorked += hoursWorked;
+	}
+
+	public double RecieveWage()
+	{
+		Console.WriteLine($"The wage for {NumberOfHoursWorked} hours worked is {Wage}");
+		ResetNumberOfHoursWorkedAfterRecieveWage();
+
+		return Wage;
+	}
+
+	private void ResetNumberOfHoursWorkedAfterRecieveWage()
+	{
+		NumberOfHoursWorked = 0;
+	}
+}
