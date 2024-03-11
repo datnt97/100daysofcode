fc3d9828172fcb0caa90fbce6754f05ffa2a5092
Nguyễn Tấn Đạt <datnt3009@gmail.com>
2024-03-11 22:12:10 +0700

    Add async demo app

---
+++ src/AsyncDemo/AsyncDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Main - 1 - Thread ID= {Environment.CurrentManagedThreadId}");
+
+
+Task t = Task.Run(AsyncFunc1);
+
+await AsyncFunc2();
+
+Console.WriteLine($"Main - 2 - Thread ID= {Environment.CurrentManagedThreadId}");
+
+t.Wait();
+
+static int AsyncFunc1()
+{
+    Console.WriteLine($"AsyncFunc1 - Thread ID = {Environment.CurrentManagedThreadId}");
+
+    return 0;
+}
+
+static async Task<int> AsyncFunc2()
+{
+    Console.WriteLine($"AsyncFunc2 - 1 Thread ID = {Environment.CurrentManagedThreadId}");
+
+    //var r = (await File.ReadAllTextAsync("sample.txt")).Length;
+
+    Console.WriteLine($"AsyncFunc2 - 2 Thread ID = {Environment.CurrentManagedThreadId}");
+
+    // Thread.Sleep(100);
+    //Console.WriteLine($"AsyncFunc2 - 3 Thread ID = {Environment.CurrentManagedThreadId}");
+    //return r;
+    return 1;
+}
